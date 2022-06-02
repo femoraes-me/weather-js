48864fad225c57291fc5254217fb33a6d39cc2ae
Felipe Moraes <em.felipe@gmail.com>
2022-06-01 23:04:43 -0300

    feito storage da localização

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// Instanciando objeto storage
+const storage = new Storage();
+
+// Pegando localização armazenada
+const weatherLocation = storage.getLocationData();
+
 // Instanciando objeto weather
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Instanciando objeto UI
 const ui = new UI();
@@ -7,7 +13,19 @@ const ui = new UI();
 // Pegar clima ao carregar DOM
 document.addEventListener('DOMContentLoaded', getWeather);
 
-//weather.changeLocation('', '');
+// evento para mudar localização
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
+
+    getWeather();
+
+    // Fechar o Modal
+    $('#locModal').modal('hide');
+});
 
 function getWeather() {
     weather.getWeather()
